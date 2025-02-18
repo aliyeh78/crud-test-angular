14b663836d06afeb67b762ca5bd828a5a0b0e229
aliyeh ansarian <aliyehansarian@gmail.com>
2025-02-18 23:51:58 +0330

    customer crud base structure

--- src/app/core/models/customer.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Customer {
-  FirstName: string;
-  LastName: string;
-  DateOfBirth: string;
-  PhoneNumber: string;
-  email: string;
-  Id:number;
-  BankAccountNumber: string;
-}
